272c159c9fc405c8d3d04e0bacce91f9f43689b9
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2018-07-27 11:20:29 +0200

    Simplify PDH class

--- FPIControl/PDH.h
+++ FPIControl/PDH.h
@@ -5,31 +5,18 @@
 #include <complex>
 #include "generalmath.h"
 
-const std::complex<double> imaginary(0.0, 1.0);
-
 class PDH {
 	public:
-		double getError(std::vector<double> data, std::vector<double> reference) {
+		template <typename T = double>
+		static T getError(std::vector<T> data, std::vector<T> reference) {
 			
-			std::vector<double> tmp; 
-			tmp.resize(data.size());
-
-			for (int j(0); j < data.size(); j++) {
-				tmp[j] = data[j] * reference[j];
-			}
+			std::vector<T> tmp(data.size());
 			
-			return generalmath::mean(tmp);
-		};
-
-		int32_t getError(std::vector<int32_t> data, std::vector<int32_t> reference) {
-
-			std::vector<int32_t> tmp;
-			tmp.resize(data.size());
-
-			for (int j(0); j < data.size(); j++) {
-				tmp[j] = data[j] * reference[j];
-			}
-
+			std::transform(data.begin(), data.end(),
+				reference.begin(), tmp.begin(),
+				std::multiplies<T>()
+			);
+			
 			return generalmath::mean(tmp);
 		};
 };
